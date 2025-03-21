959460b3c09d02259030ecec3152402449974e9a
rinya7 <v.dmitriev0712@gmail.com>
2025-03-21 12:31:35 +0100

    edit Hero

--- src/components/01_Hero.tsx
+++ src/components/01_Hero.tsx
@@ -5,9 +5,11 @@ function Hero() {
   return (
     <section>
   <div className="w-full bg-hero bg-cover bg-center border-t-4 border-green-800 flexCenter text-white text-center aspect-[4/3] min-h-[320px] md:aspect-auto md:h-screen">
+    <div className='container'>
     <div className="bg-black/50 p-6 rounded-2xl max-w-5xl mx-auto">
       <h1 className="m-0 text-4xl md:text-6xl font-bold mb-4">Hockey Club Alta Badia</h1>
       <p className="text-lg md:text-xl">Passion. Teamwork. Excellence.</p>
+    </div>
     </div>
   </div>
 </section>
